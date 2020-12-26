d9e70abf1aeb9b53a157595c022fb71df4b3e913
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-12-26 03:13:48 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MenuBar from "./component/MenuBar/";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MenuBar></MenuBar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
